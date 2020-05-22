f2406a538034e52814c8f83a9a623512b3112edd
Danielle Tobler <danielle.tobler210@gmail.com>
2020-05-22 14:09:37 -0500

    Initial commit - added skeleton and maven dependencies'

---
+++ src/main/java/dev/techtrek/techtrek/TechTrekApplication.java
@@ -0,0 +1,13 @@
+package dev.techtrek.techtrek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTrekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechTrekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/techtrek/techtrek/TechTrekApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.techtrek.techtrek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTrekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
