8d3ab1fccfb8694c1817b38b38be5185f8c14b4c
Brian Purnell <purnellbp@gmail.com>
2020-05-26 13:57:23 -0500

    updated index mapping

--- src/main/java/dev/techtrek/techtrek/repositories/UsersRepo.java
+++ src/main/java/dev/techtrek/techtrek/repositories/UsersRepo.java
@@ -5,7 +5,7 @@ import dev.techtrek.techtrek.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersRepo extends JpaRepository<User, Long> {
-        User findByUsername(String username);
-    }
+    User findByUsername(String username);
+}
 
 
