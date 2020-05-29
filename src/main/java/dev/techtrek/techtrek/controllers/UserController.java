8d014d605b338517d43810839eb088c2a2c64231
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-05-29 13:43:54 -0500

    removed unnecessary vars, imports

--- src/main/java/dev/techtrek/techtrek/controllers/UserController.java
+++ src/main/java/dev/techtrek/techtrek/controllers/UserController.java
@@ -2,21 +2,12 @@ package dev.techtrek.techtrek.controllers;
 
 
 import dev.techtrek.techtrek.models.User;
-import dev.techtrek.techtrek.repositories.UsersRepo;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
-    private UsersRepo users;
-    private PasswordEncoder passwordEncoder;
-
-    public UserController(UsersRepo users, PasswordEncoder passwordEncoder) {
-        this.users = users;
-        this.passwordEncoder = passwordEncoder;
-    }
 
     @GetMapping("/")
     public String showSignupForm(Model model){
