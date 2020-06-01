cc844486ea39cef63fa231c98337f4d5eb2b3d79
Brian Purnell <purnellbp@gmail.com>
2020-06-01 16:04:10 -0500

    updated sql schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,152 +6,119 @@
 CREATE DATABASE IF NOT EXISTS `techtrek_db`;
 USE `techtrek_db`;
 
--- -----------------------------------------------------
--- cohorts table
--- -----------------------------------------------------
+create schema if not exists techtrek_db collate utf8_general_ci;
 
-CREATE TABLE IF NOT EXISTS `cohorts`
+create table if not exists cohorts
 (
-    `id`              BIGINT(20)   NOT NULL AUTO_INCREMENT,
-    `name`            VARCHAR(255) NOT NULL,
-    `graduation_date` DATETIME(6)  NULL DEFAULT NULL,
-    `location`        VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE (`name`)
+    id bigint auto_increment
+        primary key,
+    graduation_date datetime(6) null,
+    location varchar(255) null,
+    name varchar(255) not null,
+    constraint UK_swqqq9kaugmjhmc7ans7h2kwj
+        unique (name)
 );
 
--- -----------------------------------------------------
--- users table
--- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `users`
+create table if not exists companies
 (
-    `id`                BIGINT(20)   NOT NULL AUTO_INCREMENT,
-    `bio_summary`       VARCHAR(500) NULL DEFAULT NULL,
-    `create_date`       DATETIME(6)  NULL DEFAULT NULL,
-    `email`             VARCHAR(255) NOT NULL,
-    `first_name`        VARCHAR(255) NOT NULL,
-    `github_username`   VARCHAR(255) NULL DEFAULT NULL,
-    `last_name`         VARCHAR(255) NOT NULL,
-    `linkedin_username` VARCHAR(255) NULL DEFAULT NULL,
-    `modify_date`       DATETIME(6)  NULL DEFAULT NULL,
-    `user_password`     VARCHAR(255) NOT NULL,
-    `phone_number`      VARCHAR(255) NULL DEFAULT NULL,
-    `employment_status` INT(11)      NULL DEFAULT NULL,
-    `cohort_id`         BIGINT(20)   NULL DEFAULT NULL,
-    `role`              BIGINT(20)   NULL DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE (`email`),
-    FOREIGN KEY (`role`) REFERENCES `roles` (`id`),
-    FOREIGN KEY (`cohort_id`) REFERENCES `cohorts` (`id`)
+    id bigint auto_increment
+        primary key,
+    name varchar(255) not null
 );
 
--- -----------------------------------------------------
--- roles table
--- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `roles`
+create table if not exists job_categories
 (
-    `id`       BIGINT(20)   NOT NULL AUTO_INCREMENT,
-    `name`     VARCHAR(255) NOT NULL ,
-    `users_id` BIGINT(20)   NOT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
+    id bigint auto_increment
+        primary key,
+    name varchar(255) not null,
+    constraint UK_qyol2j6hcspl3raexk33q8067
+        unique (name)
 );
 
--- -----------------------------------------------------
--- companies table
--- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `companies`
+create table if not exists points_of_contact
 (
-    `id`   BIGINT(20)   NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
+    id bigint auto_increment
+        primary key,
+    email varchar(255) not null,
+    first_name varchar(255) not null,
+    last_name varchar(255) not null,
+    phone_number varchar(255) null,
+    company_id bigint null,
+    constraint UK_j3cm4k7gxkjgtypd6ci7gdxtr
+        unique (email),
+    constraint FKbomjwn9oeb0wu2l41wu7wkpu9
+        foreign key (company_id) references companies (id)
 );
 
--- -----------------------------------------------------
--- event_listings table
--- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `event_listings`
+create table if not exists roles
 (
-    `id`           BIGINT(20)    NOT NULL AUTO_INCREMENT,
-    `date`         DATETIME(6)   NULL DEFAULT NULL,
-    `description`  TEXT          NOT NULL,
-    `is_archived`  BIT(1)        NOT NULL,
-    `listing_date` DATETIME(6)   NOT NULL,
-    `location`     VARCHAR(255)  NULL DEFAULT NULL,
-    `rsvp_url`     VARCHAR(2000) NULL DEFAULT NULL,
-    `time`         DATETIME(6)   NULL DEFAULT NULL,
-    `title`        VARCHAR(255)  NOT NULL,
-    `user_id`      BIGINT(20)    NULL DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+    id bigint auto_increment
+        primary key,
+    name varchar(255) not null,
+    constraint UK_ofx66keruapi6vyqpv6f2or37
+        unique (name)
 );
 
-
--- -----------------------------------------------------
--- job_categories table
--- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `job_categories`
+create table if not exists users
 (
-    `id`   BIGINT(20)   NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE (`name`)
+    id bigint auto_increment
+        primary key,
+    bio_summary varchar(500) null,
+    create_date datetime(6) null,
+    email varchar(255) not null,
+    employment_status int null,
+    first_name varchar(255) not null,
+    github_username varchar(255) null,
+    last_name varchar(255) not null,
+    linkedin_username varchar(255) null,
+    modify_date datetime(6) null,
+    user_password varchar(255) not null,
+    phone_number varchar(255) null,
+    cohort_id bigint null,
+    role_id bigint null,
+    constraint UK_6dotkott2kjsp8vw4d0m25fb7
+        unique (email),
+    constraint FK22vef11nher1pinpuv18ms2d3
+        foreign key (cohort_id) references cohorts (id),
+    constraint FKp56c1712k691lhsyewcssf40f
+        foreign key (role_id) references roles (id)
 );
 
--- -----------------------------------------------------
--- Table job_listings
--- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `job_listings`
+create table if not exists event_listings
 (
-    `id`               BIGINT(20)    NOT NULL AUTO_INCREMENT,
-    `apply_url`        VARCHAR(2000) NOT NULL,
-    `description`      TEXT          NOT NULL,
-    `is_archived`      BIT(1)        NOT NULL,
-    `is_remote`        BIT(1)        NOT NULL,
-    `listing_date`     TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `location`         VARCHAR(255)  NOT NULL,
-    `preferred_skills` VARCHAR(3000) NULL     DEFAULT NULL,
-    `required_skills`  VARCHAR(3000) NULL     DEFAULT NULL,
-    `title`            VARCHAR(255)  NOT NULL,
-    `user_id`          BIGINT(20)    NULL     DEFAULT NULL,
-    `company_id`       BIGINT(20)    NULL     DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`),
-    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+    id bigint auto_increment
+        primary key,
+    date datetime(6) null,
+    description text not null,
+    is_archived bit not null,
+    listing_date datetime(6) not null,
+    location varchar(255) null,
+    rsvp_url varchar(2000) null,
+    time datetime(6) null,
+    title varchar(255) not null,
+    user_id bigint null,
+    constraint FKg2ktcw4brmlljqxr3ikdo55wy
+        foreign key (user_id) references users (id)
 );
 
--- -----------------------------------------------------
--- points_of_contact table
--- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `points_of_contact`
+create table if not exists job_listings
 (
-    `id`           BIGINT(20)   NOT NULL AUTO_INCREMENT,
-    `email`        VARCHAR(255) NOT NULL,
-    `first_name`   VARCHAR(255) NOT NULL,
-    `last_name`    VARCHAR(255) NOT NULL,
-    `phone_number` VARCHAR(255) NULL DEFAULT NULL,
-    `company_id`   BIGINT(20)   NULL DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE (`email`),
-    FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`)
+    id bigint auto_increment
+        primary key,
+    apply_url varchar(2000) not null,
+    description text not null,
+    is_archived bit not null,
+    is_remote bit not null,
+    listing_date datetime(6) not null,
+    location varchar(255) not null,
+    preferred_skills varchar(3000) null,
+    required_skills varchar(3000) null,
+    title varchar(255) not null,
+    company_id bigint null,
+    user_id bigint null,
+    constraint FK7otqccbilxtfg2eyer0gs2fq2
+        foreign key (user_id) references users (id),
+    constraint FKnsya1yao8u5t25yq9tngy6xv8
+        foreign key (company_id) references companies (id)
 );
 
--- -----------------------------------------------------
--- job_listings_job_categories_join table
--- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `job_listings_job_categories_join`
-(
-    `job_listings_id`   BIGINT(20) NOT NULL,
-    `job_categories_id` BIGINT(20) NOT NULL,
-    PRIMARY KEY (`job_listings_id`, `job_categories_id`),
-    FOREIGN KEY (`job_listings_id`) REFERENCES `job_listings` (`id`),
-    FOREIGN KEY (`job_categories_id`) REFERENCES `job_categories` (`id`)
-)
