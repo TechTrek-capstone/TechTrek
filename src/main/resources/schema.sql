02004fd3540b7e9718ab8deb82f4f473d4b23080
Brian Purnell <purnellbp@gmail.com>
2020-06-01 15:37:00 -0500

    schema correction

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -53,7 +53,7 @@ CREATE TABLE IF NOT EXISTS `users`
 CREATE TABLE IF NOT EXISTS `roles`
 (
     `id`       BIGINT(20)   NOT NULL AUTO_INCREMENT,
-    `name`     VARCHAR(255) NOT NULL DEFAULT "Unassigned",
+    `name`     VARCHAR(255) NOT NULL ,
     `users_id` BIGINT(20)   NOT NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
