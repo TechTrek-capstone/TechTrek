b11129fef10cb50d4b1906cda4fcf83fbd77b1e7
Danielle Tobler <danielle.tobler210@gmail.com>
2020-06-02 12:14:02 -0500

    Created form to create company and edit company. Need filestack to be able to add company logo. Will require DB alterations to companies table

---
+++ src/main/java/dev/techtrek/techtrek/controllers/CompanyController.java
@@ -0,0 +1,67 @@
+package dev.techtrek.techtrek.controllers;
+
+import dev.techtrek.techtrek.models.*;
+import dev.techtrek.techtrek.repositories.CompaniesRepo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class CompanyController {
+
+    // Dependency injection
+    private CompaniesRepo companiesRepo;
+
+    public CompanyController(CompaniesRepo companiesRepo) {
+        this.companiesRepo = companiesRepo;
+    }
+
+// (Placement roles only) Add a company
+
+    // View the company creation form
+    @GetMapping(path = "/companies/create")
+    public String viewCreateCompanyForm(Model model) {
+        model.addAttribute("company", new Company());
+        return "companies/create";
+    }
+
+    // Create the company
+    @PostMapping(path = "/companies/create")
+    public String createJobListing(@ModelAttribute Company company) {
+
+        // Save the company to DB and redirect to companies index page (table of all companies)
+        companiesRepo.save(company);
+        return "redirect:/companies";
+    }
+
+
+// (Placement roles only) Edit a company
+
+    // View the company info to be edited
+    @GetMapping("/companies/{id}/edit")
+    public String companyEditForm(@PathVariable long id, Model model) {
+        model.addAttribute("company", companiesRepo.getOne(id));
+        return "companies/edit";
+    }
+
+    // Submit the edited post information and update the DB
+    @PostMapping("/companies/{id}/edit")
+    public String postEdit(@PathVariable long id, @RequestParam(name = "name") String name) {
+
+        // Get the company from DB
+        Company company = companiesRepo.getCompanyById(id);
+
+        // Set the name to the value in the name
+        company.setName(name);
+
+
+        // TODO: Set the image to the value in the image field
+//        company.setImg(img); // Add img to RequestParam
+
+        // Update the company in the DB
+        companiesRepo.save(company);
+
+        // Redirect to the companies index
+        return "redirect:/companies";
+    }
+}
