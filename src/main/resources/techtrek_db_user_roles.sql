d33e34d47a6e2f5efab3443ae28d01334f3d40a6
Brian Purnell <purnellbp@gmail.com>
2020-06-08 11:15:45 -0500

    added examples to seeder file for demo

--- src/main/resources/techtrek_db_user_roles.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO techtrek_db.user_roles (id, name) VALUES (1, 'Administrator');
-INSERT INTO techtrek_db.user_roles (id, name) VALUES (2, 'Placement Team');
-INSERT INTO techtrek_db.user_roles (id, name) VALUES (3, 'Student');
