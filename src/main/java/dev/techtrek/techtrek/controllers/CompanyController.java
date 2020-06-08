042b72dc26b4d5ae7da4ad328b4743ebcd85b0a7
Brian Purnell <purnellbp@gmail.com>
2020-06-07 19:58:00 -0500

    added user to job create mapping

--- src/main/java/dev/techtrek/techtrek/controllers/CompanyController.java
+++ src/main/java/dev/techtrek/techtrek/controllers/CompanyController.java
@@ -2,6 +2,8 @@ package dev.techtrek.techtrek.controllers;
 
 import dev.techtrek.techtrek.models.*;
 import dev.techtrek.techtrek.repositories.CompaniesRepo;
+import dev.techtrek.techtrek.repositories.Users;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +12,13 @@ import java.util.List;
 
 @Controller
 public class CompanyController {
-
+private Users users;
     // Dependency injection
     private CompaniesRepo companiesRepo;
 
-    public CompanyController(CompaniesRepo companiesRepo) {
+    public CompanyController(CompaniesRepo companiesRepo, Users users) {
         this.companiesRepo = companiesRepo;
+        this.users = users;
     }
 
 // (Placement roles only) View all companies
@@ -24,6 +27,9 @@ public class CompanyController {
     public String viewAllCompanies(Model model){
         List<Company> companyList = companiesRepo.findAll();
         model.addAttribute("companies", companyList);
+        UserWithRoles userWithRoles = (UserWithRoles) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = users.getOne(userWithRoles.getId());
+        model.addAttribute("user", user);
         return "companies/index";
     }
 
@@ -33,6 +39,9 @@ public class CompanyController {
     @GetMapping(path = "/companies/create")
     public String viewCreateCompanyForm(Model model) {
         model.addAttribute("company", new Company());
+        UserWithRoles userWithRoles = (UserWithRoles) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = users.getOne(userWithRoles.getId());
+        model.addAttribute("user", user);
         return "companies/create";
     }
 
@@ -52,6 +61,9 @@ public class CompanyController {
     @GetMapping("/companies/{id}/edit")
     public String companyEditForm(@PathVariable long id, Model model) {
         model.addAttribute("company", companiesRepo.getOne(id));
+        UserWithRoles userWithRoles = (UserWithRoles) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = users.getOne(userWithRoles.getId());
+        model.addAttribute("user", user);
         return "companies/edit";
     }
 
